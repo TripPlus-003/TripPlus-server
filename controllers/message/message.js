488c6e75236ee0130af1770c5e9e2d0427692c1b
Wendy03 <wendysu03@gmail.com>
2023-06-20 20:46:50 +0800

    feat: 訊息分頁

--- controllers/message/message.js
+++ controllers/message/message.js
@@ -2,13 +2,18 @@ const ObjectId = require('mongoose').Types.ObjectId;
 const successHandler = require('../../services/successHandler');
 const appError = require('../../services/appError');
 const handleErrorAsync = require('../../services/handleErrorAsync');
+const isPositiveInteger = require('../../helper/isPositiveInteger');
 const Message = require('../../models/messagesModel');
 const Room = require('../../models/roomsModel');
 const Project = require('../../models/projectsModel');
 
+const defaultPageSize = 10;
+const defaultPageIndex = 1;
+
 const createMessages = handleErrorAsync(async (req, res, next) => {
   const { receiver, content, projectId, roomId } = req.body;
   const { id } = req.user;
+  const { pageIndex, pageSize } = req.query;
   if (!receiver || !content || !roomId) {
     return next(
       appError(400, '以下欄位不可爲空：接收者 id、聊天訊息、專案 id 不可為空')
@@ -28,6 +33,13 @@ const createMessages = handleErrorAsync(async (req, res, next) => {
 const getMemberMessages = handleErrorAsync(async (req, res, next) => {
   const { id } = req.user;
   const { roomId } = req.params;
+  const { pageIndex, pageSize } = req.query;
+  const currentPageIndex = isPositiveInteger(pageIndex)
+    ? pageIndex
+    : defaultPageIndex;
+  const currentPageSize = isPositiveInteger(pageSize)
+    ? pageSize
+    : defaultPageSize;
   const messages = await Message.find({
     $and: [
       { roomId },
@@ -51,13 +63,22 @@ const getMemberMessages = handleErrorAsync(async (req, res, next) => {
         select: 'title creator'
       }
     })
-    .sort({ createdAt: -1 });
+    .sort({ createdAt: -1 })
+    .skip((currentPageIndex - 1) * currentPageSize)
+    .limit(currentPageSize);
 
   successHandler(res, '取得訊息', messages);
 });
 const getProjectMessages = handleErrorAsync(async (req, res, next) => {
   const { projectId } = req.params;
   const { id } = req.user;
+  const { pageIndex, pageSize } = req.query;
+  const currentPageIndex = isPositiveInteger(pageIndex)
+    ? pageIndex
+    : defaultPageIndex;
+  const currentPageSize = isPositiveInteger(pageSize)
+    ? pageSize
+    : defaultPageSize;
   if (!projectId || !ObjectId.isValid(projectId)) {
     return next(appError(400, '路由資訊錯誤'));
   }
@@ -99,7 +120,9 @@ const getProjectMessages = handleErrorAsync(async (req, res, next) => {
         }
       }
     })
-    .sort({ createdAt: -1 });
+    .sort({ createdAt: -1 })
+    .skip((currentPageIndex - 1) * currentPageSize)
+    .limit(currentPageSize);
 
   if (!messages || messages.length === 0) {
     return next(appError(500, '查無相關訊息'));
@@ -112,6 +135,13 @@ const getProjectMessages = handleErrorAsync(async (req, res, next) => {
 const getAdminProjectMessages = handleErrorAsync(async (req, res, next) => {
   const { projectId, roomId } = req.params;
   const { id } = req.user;
+  const { pageIndex, pageSize } = req.query;
+  const currentPageIndex = isPositiveInteger(pageIndex)
+    ? pageIndex
+    : defaultPageIndex;
+  const currentPageSize = isPositiveInteger(pageSize)
+    ? pageSize
+    : defaultPageSize;
   if (
     !projectId ||
     !ObjectId.isValid(projectId) ||
@@ -151,7 +181,9 @@ const getAdminProjectMessages = handleErrorAsync(async (req, res, next) => {
         select: 'title creator'
       }
     })
-    .sort({ createdAt: -1 });
+    .sort({ createdAt: -1 })
+    .skip((currentPageIndex - 1) * currentPageSize)
+    .limit(currentPageSize);
 
   if (!messages || messages.length === 0) {
     return next(appError(500, '查無相關訊息'));
